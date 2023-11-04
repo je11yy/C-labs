4170e7f027a6400539bbb61aac80ab020c9266e1
желешка <110609402+je11yy@users.noreply.github.com>
2023-11-04 19:29:37 +0300

    Add files via upload

--- lab3/1/main.c
+++ lab3/1/main.c
@@ -13,13 +13,15 @@ enum RESULTS
 void print_error(int error);
 
 int number_to_base(char ** result, int number, int r);
-int get_size(int power);
-int reduce_number(int number, int how_much);
 char make_char_number(int number);
+int summary (int first, int second);
+void reverse(char * string);
+int decrease(int first, int second);
+int negative(int number);
 
 int main()
 {
-    int r = 2;
+    int r = 5;
     FILE * file = fopen("tests.txt", "r");
     int result;
     char * string = NULL;
@@ -52,28 +54,10 @@ int main()
     return success;
 }
 
-int get_size(int power)
-{
-    int result = 1;
-    for (int i = 0; i < power; ++i) result <<= 1;
-    return result;
-}
-
-int reduce_number(int number, int how_much)
-{
-    for (int i = 0; i < how_much; ++i) number = ~-number;
-    return number;
-}
-
 char make_char_number(int number)
 {
-    if (number > 9)
-    {
-        number = reduce_number(number, 10);
-        for (int i = 0; i < 'A'; ++i) number = -~number;
-    }
-    else for (int i = 0; i < '0'; ++i) number = -~number;
-    return number;
+    if (number < 10) return summary(number, '0');
+    else return decrease(summary(number, 'A'), 10);
 }
 
 void reverse(char * string)
@@ -86,6 +70,28 @@ void reverse(char * string)
         string[i] = string[j];
         string[j] = temp;
     }
+}
+
+int negative(int number)
+{
+    return summary(~number, 1);
+}
+
+int decrease(int first, int second)
+{
+    return summary(first, negative(second));
+}
+
+int summary(int first, int second)
+{
+    int carry;
+    while (second)
+    {
+        carry = first & second;
+        first ^= second;
+        second = carry << 1;
+    }
+    return first;
 }
 
 int number_to_base(char ** result, int number, int r)
@@ -103,24 +109,24 @@ int number_to_base(char ** result, int number, int r)
     if (number < 0)
     {
         flag = 1;
-        number = abs(number);
+        number = negative(number);
     }
 
     int length = 2;
-    *result = (char*)malloc(length * sizeof(char)); // максимальный размер 2^5
+    *result = (char*)malloc(length * sizeof(char));
     if (*result == NULL) return no_memory;
     char * temp;
 
-    int bit_size = ~-get_size(r);
+    int bit_size = decrease(1 << r, 1);
     int remainder;
     char symbol;
     int count = 0;
 
-    while (number > 0)
+    while (number)
     {
         if (length <= count)
         {
-            length *= 2;
+            length = summary(length, 1);
             temp = (char*)realloc(*result, length * sizeof(char));
             if (!temp)
             {
@@ -130,19 +136,20 @@ int number_to_base(char ** result, int number, int r)
             }
             *result = temp;
         }
+        
         remainder = number & bit_size; // остаток от деления
 
         symbol = make_char_number(remainder);
         (*result)[count] = symbol;
 
-        number = number >> r; // делим число на r-ую степень двойки
-        count++;
+        number >>= r; // делим число на r-ую степень двойки
+        count = summary(count, 1);
     }
     if (flag)
     {
         if (length <= count)
         {
-            length *= 2;
+            length = summary(length, 1);
             temp = (char*)realloc(*result, length * sizeof(char));
             if (!temp)
             {
@@ -153,7 +160,7 @@ int number_to_base(char ** result, int number, int r)
             *result = temp;
         }
         (*result)[count] = '-';
-        count++;
+        count = summary(count, 1);
     }
     (*result)[count] = 0;
     reverse(*result);
