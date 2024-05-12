01eb5bbd47b7baf0379ec7ca09ddee80599af453
je11yy <l4da-vlasov@yandex.ru>
2024-05-12 15:43:48 +0300

    Trie test added

--- lab4/9/storages/departments/tests.c
+++ lab4/9/storages/departments/tests.c
@@ -4,6 +4,7 @@
 #include "BinarySearchTree.h"
 #include "DynamicArray.h"
 #include "HashSet.h"
+#include "Trie.h"
 
 void BST_test()
 {
@@ -136,11 +137,55 @@ void Hash_Set_test()
     printf("[HASH SET] PASSED\n");
 }
 
+void Trie_test()
+{
+    Trie_ptr tree = Trie_create();
+    
+    Application_storage_ptr storage_1 = application_storage_create(BinaryHeap_T);
+    Department_ptr dep_1 = department_create(123, storage_1, 2, 1.2);
+
+    Application_storage_ptr storage_2 = application_storage_create(LeftistHeap_T);
+    Department_ptr dep_2 = department_create(1233, storage_2, 10, 1.5);
+
+    Application_storage_ptr storage_3 = application_storage_create(SkewHeap_T);
+    Department_ptr dep_3 = department_create(1232, storage_3, 20, 1.3);
+
+    Application_storage_ptr storage_4 = application_storage_create(BinomialHeap_T);
+    Department_ptr dep_4 = department_create(12143, storage_4, 8, 1.2);
+    
+    Application_storage_ptr storage_5 = application_storage_create(Treap_T);
+    Department_ptr dep_5 = department_create(12183, storage_5, 5, 1.9);
+
+    status st;
+    st = Trie_insert(tree, 123, dep_1);
+    if (st != success) printf("[TRIE] insertion failed\n");
+    
+    st = Trie_insert(tree, 1233, dep_2);
+    if (st != success) printf("[TRIE] insertion failed\n");
+    st = Trie_insert(tree, 1232, dep_3);
+    if (st != success) printf("[TRIE] insertion failed\n");
+    st = Trie_insert(tree, 12143, dep_4);
+    if (st != success) printf("[TRIE] insertion failed\n");
+    st = Trie_insert(tree, 12183, dep_5);
+    if (st != success) printf("[TRIE] insertion failed\n");
+
+    Department_ptr dep = NULL;
+    st = Trie_find(tree, 1232, &dep);
+    if (st != success) printf("[TRIE] find failed\n");
+    if (dep->free_operators->size != 20) printf("[BST] wrong operators count\n");
+    if (dep->identifier != 1232) printf("[TRIE] wrong identifier\n");
+
+    Trie_free(tree);
+
+    printf("[TRIE] PASSED\n");
+}
+
 void run_tests()
 {
     BST_test();
     Dynamic_Array_test();
     Hash_Set_test();
+    Trie_test();
 }
 
 int main()
