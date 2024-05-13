a19384c3d6c08f7bfd61eec1c7564ce8b83a6473
je11yy <l4da-vlasov@yandex.ru>
2024-05-13 14:33:48 +0300

    Errors updated with new errors

--- lab4/9/errors.h
+++ lab4/9/errors.h
@@ -13,7 +13,11 @@ typedef enum
     invalid_argument,
     file_open_error,
     invalid_info_file,
-    invalid_function_argument
+    invalid_function_argument,
+    no_free_operators,
+    already_existing_key,
+    cannot_find
+
 } status;
 
 void print_help_menu();
